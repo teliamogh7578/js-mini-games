f92f6c53fc71face3cd6e2e93e8bed90074cff2d
Amogh Teli <teliamogh7578@gmail.com>
2026-01-23 00:18:22 +0530

    add the score track feature

--- games/guess_num/script.js
+++ games/guess_num/script.js
@@ -2,6 +2,8 @@
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector(".number").textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
@@ -13,7 +15,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🥳Correct number!";
   } else if (secretNumber > guess) {
     document.querySelector(".message").textContent = "📉Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (secretNumber < guess) {
+    score--;
+    document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "📈Too high!";
   }
 });
