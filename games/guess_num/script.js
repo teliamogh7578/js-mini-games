a10123147191b98743b7baf31078b1d5a65cc005
Amogh Teli <teliamogh7578@gmail.com>
2026-01-22 22:46:15 +0530

    Add check button functionality

---
+++ games/guess_num/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let guess;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+});
+if (!guess) {
+  document.querySelector(".message").textContent = "⛔No number!";
+  console.log("ok");
+}
