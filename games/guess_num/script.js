832fd04255b194010cfed6c1efa3619a20f261d8
Amogh Teli <teliamogh7578@gmail.com>
2026-01-23 02:11:57 +0530

    Add highscore logic

--- games/guess_num/script.js
+++ games/guess_num/script.js
@@ -1,6 +1,8 @@
 "use strict";
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -13,6 +15,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (secretNumber > guess) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉Too low!";
