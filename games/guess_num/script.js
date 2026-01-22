877ac8908c6af2d1700f04de1748e53e28669da7
Amogh Teli <teliamogh7578@gmail.com>
2026-01-22 23:48:34 +0530

    add too high and too low feedback to guess number game

--- games/guess_num/script.js
+++ games/guess_num/script.js
@@ -9,5 +9,11 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent = "⛔No number!";
     console.log("ok");
+  } else if (secretNumber === guess) {
+    document.querySelector(".message").textContent = "🥳Correct number!";
+  } else if (secretNumber > guess) {
+    document.querySelector(".message").textContent = "📉Too low!";
+  } else if (secretNumber < guess) {
+    document.querySelector(".message").textContent = "📈Too high!";
   }
 });
