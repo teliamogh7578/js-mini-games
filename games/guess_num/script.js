4af74be0c032e38ca25959a86de6c08ecb4c3837
Amogh Teli <teliamogh7578@gmail.com>
2026-01-22 23:41:00 +0530

    Add random number generator between 1 and 20

--- games/guess_num/script.js
+++ games/guess_num/script.js
@@ -1,5 +1,7 @@
 "use strict";
-let guess;
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
