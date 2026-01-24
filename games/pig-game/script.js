94e6ea3dcaec70fec33b8529910ee4ddb9c692e6
Amogh Teli <teliamogh7578@gmail.com>
2026-01-24 12:01:38 +0530

    Add Switch Player logic with scores

--- games/pig-game/script.js
+++ games/pig-game/script.js
@@ -1,4 +1,6 @@
 "use strict";
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
@@ -8,7 +10,7 @@ const btnHold = document.querySelector(".btn--hold");
 const currentScoreEl = document.querySelector(".current-score");
 
 let currentScore = 0;
-
+let activePlayer = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
@@ -21,6 +23,11 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    currentScoreEl.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
